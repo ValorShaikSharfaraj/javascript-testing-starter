6de1ab89e531aed3e34b2c097e781fdd340e4d4c
Shaik SHarfaraj Ahammed <codewithsharfaraj13@gmail.com>
2025-01-27 13:14:11 +0530

    First excercide test case complete

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { describe, test, it, expect } from "vitest";
-import { max } from "../src/intro";
+import { fizzBuzz, max } from "../src/intro";
 
 describe('max', () => {
   it('should return the first argument if it is greater', () => {
@@ -20,4 +20,36 @@ describe('max', () => {
   it('should return first argument if arguments is equal', () => {
     expect(max(1, 1)).toBe(1);
   })
-})
+});
+
+describe('fizzBuzz', () => {
+  it('should return fizz when argument is divisible by 3', () => {
+    expect(fizzBuzz(3)).toBe('Fizz');
+
+    expect(fizzBuzz(6)).toBe('Fizz');
+
+    expect(fizzBuzz(9)).toBe('Fizz');
+  })
+
+  it('should return buzz when argument is divisible by 5', () => {
+    expect(fizzBuzz(5)).toBe('Buzz');
+
+    expect(fizzBuzz(10)).toBe('Buzz');
+
+    expect(fizzBuzz(25)).toBe('Buzz');
+  })
+
+  it('should return fizzbuzz when argument is divisible by 3 and 5', () => {
+    expect(fizzBuzz(15)).toBe('FizzBuzz');
+
+    expect(fizzBuzz(30)).toBe('FizzBuzz');
+
+    expect(fizzBuzz(45)).toBe('FizzBuzz');
+  })
+
+  it('should return argument when its not divisible by 3 and 5', () => {
+    expect(fizzBuzz(1)).toBe('1');
+
+    expect(fizzBuzz(13)).toBe('13');
+  })
+});
